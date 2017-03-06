57ba792d6c9aae26593aa9056efd13576b6a2822
Wolfy <s.leheup@gmail.com>
2017-03-06 11:48:56 +0100

    import student.json in studentList.js

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,15 @@
 // Write yout Javascript code in this files
+var request = new XMLHttpRequest();
+request.open('GET', "students.json", true); //récupère students.json et true = requêtes asynchrones autorisées (requete après chargement)
+request.onreadystatechange = function() {
+    if (request.readyState == 4) {
+        if (request.status == 200) { //status 200 == tout s'est bien passé
+            var students = JSON.parse(request.responseText);
+            console.log(students);
+        } else {
+            dump("Erreur pendant le chargement de la page.\n");
+        }
+    };
+}
+
+request.send();
